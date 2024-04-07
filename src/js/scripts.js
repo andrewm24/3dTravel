345ad2e5b58336178ea361cc416d5b5dddb4b475
Andrew <andrew71865@gmail.com>
2024-04-07 00:37:17 -0400

    added some stuff

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,45 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'; 
 
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
+camera.position.set(-10,30,30);
+orbit.update();
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const box = new THREE.Mesh(geometry, material);
+scene.add(box); 
+
+const planeGeometry = new THREE.PlaneGeometry(30,30);
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ffff});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+
+function animate(time) {
+    console.log(time);
+
+    box.rotation.x = time / 1000;
+    box.rotation.y = time / 1000;
+    renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
 
